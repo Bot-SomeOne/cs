ad7467e0269c9602750622b9b77779da08d1668f
Ly Tran Vinh <lytranvinh.work@gmail.com>
2024-08-25 13:13:09 +0100

    [Upgrade]: use try catch input err

--- Ex2_Day250824/Form1.cs
+++ Ex2_Day250824/Form1.cs
@@ -4,22 +4,26 @@ namespace Ex2_Day250824
 {
     public partial class Form1 : Form
     {
+        // Var
+        private bool check;
         public Form1()
         {
             InitializeComponent();
-            Input_x1.Text = "0";
-            Input_x2.Text = "0";
-            Input_y1.Text = "0";
-            Input_y2.Text = "0";
-
+            check = true;
         }
 
         private void btnTinhToan_Click(object sender, EventArgs e)
         {
-            double x1 = Convert.ToDouble(Input_x1.Text);
-            double x2 = Convert.ToDouble(Input_x2.Text);
-            double y1 = Convert.ToDouble(Input_y1.Text);
-            double y2 = Convert.ToDouble(Input_y2.Text);
+            double x1 = ConvertString(Input_x1);
+            double x2 = ConvertString(Input_x2);
+            double y1 = ConvertString(Input_y1);
+            double y2 = ConvertString(Input_y2);
+
+            if (!this.check)
+            {
+                this.check = true;
+                return;
+            }
 
             //String temp = $"x1: {x1}, y1: {y1}\nx2: {x2}, y2: {y2}";
             //MessageBox.Show(temp);
@@ -27,6 +31,24 @@ namespace Ex2_Day250824
             r_hesogoc.Text = help.Angle(x1, y1, x2, y2).ToString();
             r_khoangcach.Text = help.Distance(x1, y1, x2, y2).ToString();
 
+        }
+
+        // Help convert
+        private double ConvertString(TextBox tb)
+        {
+            try
+            {
+                double res = Double.Parse(tb.Text);
+                return res;
+            }
+            catch (FormatException fe)
+            {
+                tb.Text = "";
+                tb.PlaceholderText = "Vui long nhap dung dinh dang";
+                tb.Focus();
+                this.check = false;
+            }
+            return 0;
         }
     }
 }
