fc17d902eb61fe0d566012e6ac6508f200b9a986
Ly Tran Vinh <lytranvinh.work@gmail.com>
2024-08-25 02:29:35 +0100

    [Upgrade]: listen handle and get value ok

--- Ex2_Day250824/Form1.Designer.cs
+++ Ex2_Day250824/Form1.Designer.cs
@@ -29,20 +29,20 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
-            groupBox2 = new GroupBox();
-            x1 = new TextBox();
-            y1 = new TextBox();
-            y2 = new TextBox();
-            x2 = new TextBox();
-            label1 = new Label();
             label2 = new Label();
+            label1 = new Label();
+            Input_y1 = new TextBox();
+            Input_x1 = new TextBox();
+            groupBox2 = new GroupBox();
             label3 = new Label();
+            Input_y2 = new TextBox();
             label4 = new Label();
+            Input_x2 = new TextBox();
             groupBox3 = new GroupBox();
-            label5 = new Label();
-            r_hesogoc = new TextBox();
             r_khoangcach = new TextBox();
             label6 = new Label();
+            r_hesogoc = new TextBox();
+            label5 = new Label();
             btnTinhToan = new Button();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
@@ -53,67 +53,14 @@
             // 
             groupBox1.Controls.Add(label2);
             groupBox1.Controls.Add(label1);
-            groupBox1.Controls.Add(y1);
-            groupBox1.Controls.Add(x1);
+            groupBox1.Controls.Add(Input_y1);
+            groupBox1.Controls.Add(Input_x1);
             groupBox1.Location = new Point(103, 63);
             groupBox1.Name = "groupBox1";
             groupBox1.Size = new Size(200, 100);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Diem thu nhat";
-            groupBox1.Enter += groupBox1_Enter;
-            // 
-            // groupBox2
-            // 
-            groupBox2.Controls.Add(label3);
-            groupBox2.Controls.Add(y2);
-            groupBox2.Controls.Add(label4);
-            groupBox2.Controls.Add(x2);
-            groupBox2.Location = new Point(398, 63);
-            groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(200, 100);
-            groupBox2.TabIndex = 1;
-            groupBox2.TabStop = false;
-            groupBox2.Text = "Diem thu hai";
-            // 
-            // x1
-            // 
-            x1.Location = new Point(80, 33);
-            x1.Name = "x1";
-            x1.Size = new Size(100, 23);
-            x1.TabIndex = 2;
-            x1.TextChanged += textBox1_TextChanged;
-            // 
-            // y1
-            // 
-            y1.Location = new Point(80, 71);
-            y1.Name = "y1";
-            y1.Size = new Size(100, 23);
-            y1.TabIndex = 3;
-            // 
-            // y2
-            // 
-            y2.Location = new Point(83, 71);
-            y2.Name = "y2";
-            y2.Size = new Size(100, 23);
-            y2.TabIndex = 5;
-            // 
-            // x2
-            // 
-            x2.Location = new Point(83, 33);
-            x2.Name = "x2";
-            x2.Size = new Size(100, 23);
-            x2.TabIndex = 4;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(34, 36);
-            label1.Name = "label1";
-            label1.Size = new Size(13, 15);
-            label1.TabIndex = 4;
-            label1.Text = "x";
-            label1.Click += label1_Click;
             // 
             // label2
             // 
@@ -123,7 +70,42 @@
             label2.Size = new Size(13, 15);
             label2.TabIndex = 5;
             label2.Text = "y";
-            label2.Click += label2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(34, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(13, 15);
+            label1.TabIndex = 4;
+            label1.Text = "x";
+            // 
+            // Input_y1
+            // 
+            Input_y1.Location = new Point(80, 71);
+            Input_y1.Name = "Input_y1";
+            Input_y1.Size = new Size(100, 23);
+            Input_y1.TabIndex = 3;
+            // 
+            // Input_x1
+            // 
+            Input_x1.Location = new Point(80, 33);
+            Input_x1.Name = "Input_x1";
+            Input_x1.Size = new Size(100, 23);
+            Input_x1.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(Input_y2);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(Input_x2);
+            groupBox2.Location = new Point(398, 63);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(200, 100);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Diem thu hai";
             // 
             // label3
             // 
@@ -134,6 +116,13 @@
             label3.TabIndex = 7;
             label3.Text = "y";
             // 
+            // Input_y2
+            // 
+            Input_y2.Location = new Point(83, 71);
+            Input_y2.Name = "Input_y2";
+            Input_y2.Size = new Size(100, 23);
+            Input_y2.TabIndex = 5;
+            // 
             // label4
             // 
             label4.AutoSize = true;
@@ -142,6 +131,13 @@
             label4.Size = new Size(13, 15);
             label4.TabIndex = 6;
             label4.Text = "x";
+            // 
+            // Input_x2
+            // 
+            Input_x2.Location = new Point(83, 33);
+            Input_x2.Name = "Input_x2";
+            Input_x2.Size = new Size(100, 23);
+            Input_x2.TabIndex = 4;
             // 
             // groupBox3
             // 
@@ -155,26 +151,6 @@
             groupBox3.TabIndex = 2;
             groupBox3.TabStop = false;
             // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(70, 36);
-            label5.Name = "label5";
-            label5.Size = new Size(60, 15);
-            label5.TabIndex = 6;
-            label5.Text = "He so goc";
-            label5.Click += label5_Click;
-            // 
-            // r_hesogoc
-            // 
-            r_hesogoc.HideSelection = false;
-            r_hesogoc.Location = new Point(25, 70);
-            r_hesogoc.Name = "r_hesogoc";
-            r_hesogoc.ReadOnly = true;
-            r_hesogoc.Size = new Size(152, 23);
-            r_hesogoc.TabIndex = 6;
-            r_hesogoc.TextChanged += textBox1_TextChanged_1;
-            // 
             // r_khoangcach
             // 
             r_khoangcach.HideSelection = false;
@@ -183,7 +159,6 @@
             r_khoangcach.ReadOnly = true;
             r_khoangcach.Size = new Size(152, 23);
             r_khoangcach.TabIndex = 7;
-            r_khoangcach.TextChanged += textBox2_TextChanged;
             // 
             // label6
             // 
@@ -194,6 +169,24 @@
             label6.TabIndex = 8;
             label6.Text = "Khoang cach";
             // 
+            // r_hesogoc
+            // 
+            r_hesogoc.HideSelection = false;
+            r_hesogoc.Location = new Point(25, 70);
+            r_hesogoc.Name = "r_hesogoc";
+            r_hesogoc.ReadOnly = true;
+            r_hesogoc.Size = new Size(152, 23);
+            r_hesogoc.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(70, 36);
+            label5.Name = "label5";
+            label5.Size = new Size(60, 15);
+            label5.TabIndex = 6;
+            label5.Text = "He so goc";
+            // 
             // btnTinhToan
             // 
             btnTinhToan.Location = new Point(296, 382);
@@ -202,6 +195,7 @@
             btnTinhToan.TabIndex = 3;
             btnTinhToan.Text = "Tinh Toan";
             btnTinhToan.UseVisualStyleBackColor = true;
+            btnTinhToan.Click += btnTinhToan_Click;
             // 
             // Form1
             // 
@@ -214,7 +208,6 @@
             Controls.Add(groupBox1);
             Name = "Form1";
             Text = "Toa do cac diem";
-            Load += Form1_Load;
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             groupBox2.ResumeLayout(false);
@@ -228,10 +221,10 @@
 
         private GroupBox groupBox1;
         private GroupBox groupBox2;
-        private TextBox x1;
-        private TextBox y1;
-        private TextBox y2;
-        private TextBox x2;
+        private TextBox Input_x1;
+        private TextBox Input_y1;
+        private TextBox Input_y2;
+        private TextBox Input_x2;
         private Label label1;
         private Label label2;
         private Label label3;
--- Ex2_Day250824/Form1.cs
+++ Ex2_Day250824/Form1.cs
@@ -5,51 +5,23 @@ namespace Ex2_Day250824
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
+            Input_x1.Text = "0";
+            Input_x2.Text = "0";
+            Input_y1.Text = "0";
+            Input_y2.Text = "0";
 
         }
 
-        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        private void btnTinhToan_Click(object sender, EventArgs e)
         {
+            double x1 = Convert.ToDouble(Input_x1.Text);
+            double x2 = Convert.ToDouble(Input_x2.Text);
+            double y1 = Convert.ToDouble(Input_y1.Text);
+            double y2 = Convert.ToDouble(Input_y2.Text);
 
-        }
+            String temp = $"x1: {x1}, y1: {y1}\nx2: {x2}, y2: {y2}";
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
+            MessageBox.Show(temp);
         }
     }
 }
