c25da46c0623b2f0b5a01300e2c8148209dea290
Ly Tran Vinh <lytranvinh.work@gmail.com>
2024-09-14 23:47:06 +0100

    [ADD]: Add view

---
+++ TH2_Ex2/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace TH2_Ex2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnThemVaoDS = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.txtTien = new System.Windows.Forms.TextBox();
+            this.comboBoxChonDoUong = new System.Windows.Forms.ComboBox();
+            this.comboBoxSoLuong = new System.Windows.Forms.ComboBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.txtGiaDuThuyen = new System.Windows.Forms.TextBox();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.radioCaNgay = new System.Windows.Forms.RadioButton();
+            this.radioNuaNgay = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnThemMoi = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.radioNuaNgay);
+            this.groupBox1.Controls.Add(this.radioCaNgay);
+            this.groupBox1.Controls.Add(this.txtGiaDuThuyen);
+            this.groupBox1.Controls.Add(this.txtHoTen);
+            this.groupBox1.Controls.Add(this.comboBoxSoLuong);
+            this.groupBox1.Controls.Add(this.comboBoxChonDoUong);
+            this.groupBox1.Controls.Add(this.txtTien);
+            this.groupBox1.Controls.Add(this.btnThemMoi);
+            this.groupBox1.Controls.Add(this.btnThemVaoDS);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(21, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(391, 326);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nhap Thong Tin Khach Hang Dat Tour";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtData);
+            this.groupBox2.Location = new System.Drawing.Point(430, 58);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(358, 312);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Danh Sach Khach Hang Dat Tour";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ho Ten";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Gia Du Thuyen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Chon Do Uong";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(149, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "So Luong";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(258, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Tien";
+            // 
+            // btnThemVaoDS
+            // 
+            this.btnThemVaoDS.Location = new System.Drawing.Point(35, 277);
+            this.btnThemVaoDS.Name = "btnThemVaoDS";
+            this.btnThemVaoDS.Size = new System.Drawing.Size(123, 29);
+            this.btnThemVaoDS.TabIndex = 1;
+            this.btnThemVaoDS.Text = "Them Vao DS";
+            this.btnThemVaoDS.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(707, 405);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 1;
+            this.btnThoat.Text = "Thoat";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // txtTien
+            // 
+            this.txtTien.Location = new System.Drawing.Point(261, 223);
+            this.txtTien.Name = "txtTien";
+            this.txtTien.Size = new System.Drawing.Size(92, 20);
+            this.txtTien.TabIndex = 2;
+            // 
+            // comboBoxChonDoUong
+            // 
+            this.comboBoxChonDoUong.FormattingEnabled = true;
+            this.comboBoxChonDoUong.Location = new System.Drawing.Point(19, 225);
+            this.comboBoxChonDoUong.Name = "comboBoxChonDoUong";
+            this.comboBoxChonDoUong.Size = new System.Drawing.Size(104, 21);
+            this.comboBoxChonDoUong.TabIndex = 3;
+            // 
+            // comboBoxSoLuong
+            // 
+            this.comboBoxSoLuong.FormattingEnabled = true;
+            this.comboBoxSoLuong.Location = new System.Drawing.Point(152, 224);
+            this.comboBoxSoLuong.Name = "comboBoxSoLuong";
+            this.comboBoxSoLuong.Size = new System.Drawing.Size(85, 21);
+            this.comboBoxSoLuong.TabIndex = 3;
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(101, 34);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(168, 20);
+            this.txtHoTen.TabIndex = 4;
+            // 
+            // txtGiaDuThuyen
+            // 
+            this.txtGiaDuThuyen.Location = new System.Drawing.Point(101, 129);
+            this.txtGiaDuThuyen.Name = "txtGiaDuThuyen";
+            this.txtGiaDuThuyen.Size = new System.Drawing.Size(139, 20);
+            this.txtGiaDuThuyen.TabIndex = 4;
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(6, 20);
+            this.txtData.Multiline = true;
+            this.txtData.Name = "txtData";
+            this.txtData.ReadOnly = true;
+            this.txtData.Size = new System.Drawing.Size(346, 286);
+            this.txtData.TabIndex = 4;
+            // 
+            // radioCaNgay
+            // 
+            this.radioCaNgay.AutoSize = true;
+            this.radioCaNgay.Location = new System.Drawing.Point(29, 80);
+            this.radioCaNgay.Name = "radioCaNgay";
+            this.radioCaNgay.Size = new System.Drawing.Size(66, 17);
+            this.radioCaNgay.TabIndex = 5;
+            this.radioCaNgay.TabStop = true;
+            this.radioCaNgay.Text = "Ca Ngay";
+            this.radioCaNgay.UseVisualStyleBackColor = true;
+            // 
+            // radioNuaNgay
+            // 
+            this.radioNuaNgay.AutoSize = true;
+            this.radioNuaNgay.Location = new System.Drawing.Point(196, 80);
+            this.radioNuaNgay.Name = "radioNuaNgay";
+            this.radioNuaNgay.Size = new System.Drawing.Size(73, 17);
+            this.radioNuaNgay.TabIndex = 5;
+            this.radioNuaNgay.TabStop = true;
+            this.radioNuaNgay.Text = "Nua Ngay";
+            this.radioNuaNgay.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(246, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "$";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(353, 225);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "$";
+            // 
+            // btnThemMoi
+            // 
+            this.btnThemMoi.Location = new System.Drawing.Point(205, 277);
+            this.btnThemMoi.Name = "btnThemMoi";
+            this.btnThemMoi.Size = new System.Drawing.Size(123, 29);
+            this.btnThemMoi.TabIndex = 1;
+            this.btnThemMoi.Text = "Them Moi";
+            this.btnThemMoi.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Cong Ty Du Thuyen Ho Tay";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtTien;
+        private System.Windows.Forms.Button btnThemVaoDS;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ComboBox comboBoxSoLuong;
+        private System.Windows.Forms.ComboBox comboBoxChonDoUong;
+        private System.Windows.Forms.RadioButton radioNuaNgay;
+        private System.Windows.Forms.RadioButton radioCaNgay;
+        private System.Windows.Forms.TextBox txtGiaDuThuyen;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnThemMoi;
+    }
+}
+
